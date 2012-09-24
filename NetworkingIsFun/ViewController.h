999f5867ebc192fca487e4cbdee8bc7badb50c70
beepscore <support@beepscore.com>
2012-09-23 23:35:43 -0700

    In ViewController, add properties, implement viewDidLoad and tableView datasource methods.

--- NetworkingIsFun/ViewController.h
+++ NetworkingIsFun/ViewController.h
@@ -8,6 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicatorView;
+@property (nonatomic, strong) NSArray *movies;
 
 @end
--- NetworkingIsFun/ViewController.m
+++ NetworkingIsFun/ViewController.m
@@ -17,7 +17,30 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Set Up Table View
+    self.tableView = [[UITableView alloc]
+                      initWithFrame:CGRectMake(0.0,
+                                               0.0,
+                                               self.view.bounds.size.width,
+                                               self.view.bounds.size.height)
+                      style:UITableViewStylePlain];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    
+    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth
+    | UIViewAutoresizingFlexibleHeight;
+    self.tableView.hidden = YES;
+    [self.view addSubview:self.tableView];
+    
+    // Set Up Activity Indicator View
+    self.activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle: UIActivityIndicatorViewStyleGray];
+    self.activityIndicatorView.hidesWhenStopped = YES;
+    self.activityIndicatorView.center = self.view.center;
+    [self.view addSubview:self.activityIndicatorView];
+    [self.activityIndicatorView startAnimating];
+    
+    // Initialize Data Source
+    self.movies = [[NSArray alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +48,31 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - TableView Datasource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (self.movies && self.movies.count) {
+        return self.movies.count;
+    } else {
+        return 0;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *cellID = @"Cell Identifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                      reuseIdentifier:cellID];
+    }
+    return cell;
+}
+
+#pragma mark - TableView Delegate
+//don't implement any delegate methods
+
+#pragma mark -
 
 @end
