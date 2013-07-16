be400c81107db50e805afb969666ea2a7d66b4a6
beepscore <support@beepscore.com>
2013-07-16 13:31:41 -0700

    Add ViewController_Private.h to expose methods for use by unit tests.

--- NetworkingIsFun/ViewController.m
+++ NetworkingIsFun/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ViewController_Private.h"
 #import "AFNetworking.h"
 
 @interface ViewController ()
---
+++ NetworkingIsFun/ViewController_Private.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  NetworkingIsFun
+//
+//  Created by Steve Baker on 7/16/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+//  Expose methods for use by unit tests
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+- (void)configureActivityIndicator;
+
+@end
